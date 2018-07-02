cc881c3b151f25a13dfbfa89be2f7ec25cdda42f
Roman Novikov <mystdeim@gmail.com>
2018-07-02 16:40:26 +0300

    fixes

--- src/main/kotlin/mystdeim/nanobank/service/AccountService.kt
+++ src/main/kotlin/mystdeim/nanobank/service/AccountService.kt
@@ -1,11 +1,13 @@
 package mystdeim.nanobank.service
 
 import io.vertx.codegen.annotations.ProxyGen
+import io.vertx.codegen.annotations.VertxGen
 import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
 import mystdeim.nanobank.data.Account
 
 @ProxyGen
+@VertxGen
 interface AccountService {
     fun create(account: Account, resultHandler: Handler<AsyncResult<String>>)
 
